d2cac10a0f5f73038163664b9adfa09a8cadd253
Miró Sorja <miro.sorja@digital-identity.com>
2015-04-30 11:02:50 +0300

    Some error

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,10 @@
-// Toteuta moduulisi tänne
+var MovieLibrary = angular.module('MovieLibrary', ['ngRoute, firebase']);
+MovieLibrary.config(function ($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'ListMoviesController',
+		templateUrl: 'app/views/newmovie.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
